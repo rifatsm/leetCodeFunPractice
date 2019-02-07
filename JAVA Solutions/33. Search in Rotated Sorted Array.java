508859d0a4eac8480318fd7e0924918f58d8d4f6
rifatsm <rifatsm@vt.edu>
2019-02-07 11:45:02 -0500

    33. Search in Rotated Sorted Array.java

--- JAVA Solutions/33. Search in Rotated Sorted Array.java
+++ JAVA Solutions/33. Search in Rotated Sorted Array.java
@@ -1,6 +1,44 @@
 33. Search in Rotated Sorted Array.java
 
-// Solution: Binary Search, Two pointers 
+// Solution 2: Binary Search, Two pointers 
+// Runtime: 7 ms
+// TC: O(logn)
+// SC: O(1)
+/* Approach: Binary Search
+*   1. Binary Search with additional conditional check to find in which range the "target" can be.
+*/
+
+class Solution {
+    public int search(int[] nums, int target) {
+        if(nums.length == 0 || nums == null)
+            return -1;
+        int left = 0, right = nums.length - 1, mid = 0;
+        
+        // Binary Search
+        while(left <= right){
+            mid = left + (right - left)/2;
+            if(nums[mid] == target)
+                return mid;
+            if(nums[mid] < nums[right]){
+                if(nums[mid] < target && target <= nums[right])
+                    left = mid + 1;
+                else
+                    right = mid - 1;
+            }else{
+                if(nums[left] <= target && target < nums[mid])
+                    right = mid - 1;
+                else
+                    left = mid + 1;
+            }
+        }
+        
+        
+        return -1;
+    }
+}
+
+
+// Solution 1: Binary Search, Two pointers 
 // Runtime: 6 ms 
 // TC: O(logn)
 // SC: O(1)
